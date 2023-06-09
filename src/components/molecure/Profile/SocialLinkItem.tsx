274183bae930e9eb42afdeaae628f79f38e0101b
Rashmi-278 <rashmiabbigeri@gmail.com>
2023-06-09 14:28:20 +0530

    cursor default when link disabled

--- src/components/molecure/Profile/SocialLinkItem.tsx
+++ src/components/molecure/Profile/SocialLinkItem.tsx
@@ -40,7 +40,7 @@ export const SocialLinkItem: FC<Props> = ({ linkType, value }) => {
   const IconWrapper = styled.div`
     width: 32px;
     height: 32px;
-    cursor: ${value || linkType === 'more' ? 'pointer' : 'none'};
+    cursor: ${value || linkType === 'more' ? 'pointer' : 'default'};
     @media (max-width: 599px) {
       width: 24px;
       height: 24px;
