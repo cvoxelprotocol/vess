201617ad3be69dc0e18e7ae31bacaa121e12a2b0
Rashmi-278 <rashmiabbigeri@gmail.com>
2023-08-01 15:06:09 +0530

    init _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,14 @@ import { css, Global, ThemeProvider } from '@emotion/react'
 import { Noto_Sans } from '@next/font/google'
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { DehydratedState } from '@tanstack/react-query'
-import { CHAIN_NAMESPACES } from "@web3auth/base";
-import { Web3Auth } from "@web3auth/modal";
-import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+import { CHAIN_NAMESPACES } from '@web3auth/base'
+import { Web3Auth } from '@web3auth/modal'
+import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
 import { Provider as JotaiProvider } from 'jotai'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
-import { configureChains, createClient, mainnet, WagmiConfig, createConfig } from 'wagmi'
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { configureChains, mainnet, WagmiConfig, createConfig } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import { infuraProvider } from 'wagmi/providers/infura'
@@ -40,57 +40,54 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_KEY || '' }), publicProvider()],
 )
 
+// Instantiating Web3Auth
+const web3AuthInstance = new Web3Auth({
+  clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '',
+  chainConfig: {
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    chainId: '0x' + chains[0].id.toString(16),
+    rpcTarget: chains[0].rpcUrls.default.http[0], // This is the public RPC we have added, please pass on your own endpoint while creating an app
+    displayName: chains[0].name,
+    tickerName: chains[0].nativeCurrency?.name,
+    ticker: chains[0].nativeCurrency?.symbol,
+    blockExplorer: chains[0]?.blockExplorers.default?.url,
+  },
+  uiConfig: {
+    appName: 'Vess Resume',
+    theme: 'dark',
+    defaultLanguage: 'en',
+    appLogo: 'https://web3auth.io/images/w3a-L-Favicon-1.svg', // Your App Logo Here
+    modalZIndex: '2147483647',
+  },
+  web3AuthNetwork: 'cyan',
+  enableLogging: true,
+})
 
-  // Instantiating Web3Auth
-  const web3AuthInstance = new Web3Auth({
-    clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '',
-    chainConfig: {
-      chainNamespace: CHAIN_NAMESPACES.EIP155,
-      chainId: "0x" + chains[0].id.toString(16),
-      rpcTarget: chains[0].rpcUrls.default.http[0], // This is the public RPC we have added, please pass on your own endpoint while creating an app
-      displayName: chains[0].name,
-      tickerName: chains[0].nativeCurrency?.name,
-      ticker: chains[0].nativeCurrency?.symbol,
-      blockExplorer: chains[0]?.blockExplorers.default?.url,
-    },
-    uiConfig: {
-      appName: 'Vess Resume',
-      theme: "dark",
-      defaultLanguage: "en",
-      appLogo: "https://web3auth.io/images/w3a-L-Favicon-1.svg", // Your App Logo Here
-      modalZIndex: "2147483647",
-    },
-    web3AuthNetwork: "cyan",
-    enableLogging: true,
-  });
-
-
-  const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: [
-      new Web3AuthConnector({
-        chains,
-        options: {
-          web3AuthInstance,
-        },
-      }),
-      new InjectedConnector({
-        chains,
-        options: {
-          name: "Injected",
-          shimDisconnect: true,
-        },
-      }),
-    ],
-    publicClient,
-    webSocketPublicClient,
-  });
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new Web3AuthConnector({
+      chains,
+      options: {
+        web3AuthInstance,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<{ dehydratedState: DehydratedState }>) {
-  
   const metamaskConnector = new MetaMaskConnector({ chains })
   const walletConnectConnector = new WalletConnectConnector({
     chains,
@@ -99,7 +96,7 @@ export default function App({
       showQrModal: true,
     },
   })
-  
+
   // //const client = createClient({
   //   autoConnect: true,
   //   connectors: [metamaskConnector, walletConnectConnector],
@@ -123,7 +120,7 @@ export default function App({
       <JotaiProvider>
         <QueryClientProvider client={queryClient}>
           <Hydrate state={dehydratedState}>
-            <WagmiConfig client={wagmiConfig} >
+            <WagmiConfig client={wagmiConfig}>
               <ComposeWrapper>
                 <ThemeProvider theme={theme}>
                   <GATracking trackingId={process.env.NEXT_PUBLIC_GA_ID} />
