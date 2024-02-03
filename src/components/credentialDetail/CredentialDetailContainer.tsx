6e75cfe494e533e0d04e21b5958dc268c78d11bf
Kouki Minamoto <tomatomusicstudio@gmail.com>
2024-02-03 21:25:57 +0900

    Add new pages and components

---
+++ src/components/credentialDetail/CredentialDetailContainer.tsx
@@ -0,0 +1,326 @@
+import styled from '@emotion/styled'
+import copy from 'copy-to-clipboard'
+import {
+  Text,
+  Chip,
+  FlexHorizontal,
+  IconButton,
+  Skelton,
+  Button,
+  useSnackbar,
+  Spinner,
+} from 'kai-kit'
+import { useRouter } from 'next/router'
+import { FC, useEffect, useRef, useState } from 'react'
+import { PiArrowClockwise, PiCheckCircle, PiX, PiCopyBold, PiWarning } from 'react-icons/pi'
+import { ImageContainer } from '../ui-v1/Images/ImageContainer'
+import { useDIDAccount } from '@/hooks/useDIDAccount'
+import useScrollCondition from '@/hooks/useScrollCondition'
+import { useVerifiableCredentials } from '@/hooks/useVerifiableCredentials'
+import { getVESSService } from '@/lib/vess'
+
+export type CredDetailProps = {
+  id?: string
+}
+
+export const CredentialDetailContainer: FC<CredDetailProps> = ({ id }) => {
+  const { did } = useDIDAccount()
+  const router = useRouter()
+  const { certificates, attendances, memberships, isInitialLoading, CredentialsByHolder } =
+    useVerifiableCredentials(did)
+  const { openSnackbar } = useSnackbar({
+    id: 'plain-cred-copied',
+    text: '元データ(JSON)をコピーしました。',
+  })
+  const { openSnackbar: openURLCopied } = useSnackbar({
+    id: 'share-url-copied',
+    text: '共有用URLをコピーしました。',
+  })
+  const scrollRef = useRef<HTMLDivElement>(null)
+  const { scrollInfo } = useScrollCondition(scrollRef)
+
+  const verify = async (plainCred: string) => {
+    const vessKit = getVESSService()
+    setVerified('verifying')
+    const result = await vessKit.verifyCredential(plainCred) // plainCredentialはJSON文字列
+    setTimeout(() => {
+      if (result.verified === true) {
+        setVerified('verified')
+      } else {
+        setVerified('failed')
+      }
+    }, 600)
+  }
+  const [verified, setVerified] = useState<'verified' | 'failed' | 'verifying'>('verifying')
+  useEffect(() => {
+    verify(CredentialsByHolder?.data[0]?.plainCredential || '{}')
+  }, [CredentialsByHolder?.data[0]?.plainCredential])
+
+  return (
+    <>
+      <CredDetailFrame
+        data-scroll-down={
+          (scrollInfo.direction == 'down' && scrollInfo.positionY > 100) || undefined
+        }
+      >
+        <CredImageFrame>
+          <Skelton isLoading={isInitialLoading} width='354px' height='237px' radius='24px'>
+            <ImageContainer
+              src={memberships[0]?.credentialSubject.membershipIcon}
+              // src='/sample/event_sample.jpg'
+              width='100%'
+              height='fit-content'
+              objectFit='contain'
+              maxWidth='400px'
+              maxHeight='300px'
+              style={{ flex: '0 0 auto' }}
+            />
+            <FlexHorizontal gap='var(--kai-size-sys-space-sm)'>
+              <Chip
+                variant='tonal'
+                color={
+                  verified === 'verified'
+                    ? 'success'
+                    : verified === 'verifying'
+                    ? 'neutral'
+                    : 'error'
+                }
+                startContent={
+                  verified === 'verified' ? (
+                    <PiCheckCircle size={24} />
+                  ) : verified === 'verifying' ? (
+                    <Spinner size='sm' color='neutral' />
+                  ) : (
+                    <PiWarning size={24} />
+                  )
+                }
+              >
+                {verified === 'verified'
+                  ? 'この証明書は有効です'
+                  : verified === 'verifying'
+                  ? '検証中'
+                  : 'この証明書は無効です'}
+              </Chip>
+
+              <IconButton
+                size='xs'
+                icon={<PiArrowClockwise />}
+                color='neutral'
+                variant='tonal'
+                round='sm'
+                onPress={() => verify(CredentialsByHolder?.data[0]?.plainCredential || '')}
+              />
+            </FlexHorizontal>
+          </Skelton>
+        </CredImageFrame>
+        <CredInfoFrame>
+          <div style={{ width: '100%', flex: 0 }}>
+            <Text as='h2' typo='title-lg' color='var(--kai-color-sys-on-layer)'>
+              {memberships[0]?.credentialSubject.membershipName}
+            </Text>
+          </div>
+          <InfoItemsFrame ref={scrollRef}>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                説明文
+              </Text>
+              <Text typo='body-lg' color='var(--kai-color-sys-on-layer)'>
+                (どこから取得できるかわかりませんでした。)
+              </Text>
+            </InfoItemFrame>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                発行者
+              </Text>
+              <FlexHorizontal gap='var(--kai-size-sys-space-xs)'>
+                <ImageContainer
+                  src={memberships[0]?.credentialSubject.organizationIcon}
+                  width='20px'
+                  height='20px'
+                  objectFit='contain'
+                  alt='Organization Icon'
+                />
+                <Text typo='body-lg' color='var(--kai-color-sys-on-layer)'>
+                  {memberships[0]?.credentialSubject.organizationName}
+                </Text>
+              </FlexHorizontal>
+            </InfoItemFrame>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                所有者
+              </Text>
+              <Text typo='body-lg' color='var(--kai-color-sys-on-layer)'>
+                {memberships[0]?.credentialSubject.id}
+              </Text>
+            </InfoItemFrame>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                有効開始日
+              </Text>
+              <Text typo='body-lg' color='var(--kai-color-sys-on-layer)'>
+                {memberships[0]?.credentialSubject.startDate}
+              </Text>
+            </InfoItemFrame>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                有効期限日
+              </Text>
+              <Text typo='body-lg' color='var(--kai-color-sys-on-layer)'>
+                {memberships[0]?.credentialSubject.endDate || '無期限'}
+              </Text>
+            </InfoItemFrame>
+            <InfoItemFrame>
+              <Text typo='label-lg' color='var(--kai-color-sys-on-layer-minor)'>
+                元データ(JSON)
+              </Text>
+              <PlainCredFrame>
+                <Chip
+                  variant='tonal'
+                  startContent={<PiCopyBold />}
+                  color='subdominant'
+                  onPress={() => {
+                    openSnackbar()
+                    copy(
+                      JSON.stringify(
+                        JSON.parse(CredentialsByHolder?.data[0]?.plainCredential || '{}'),
+                        null,
+                        2,
+                      ),
+                    )
+                  }}
+                  style={{
+                    position: 'absolute',
+                    top: 'var(--kai-size-sys-space-sm)',
+                    right: 'var(--kai-size-sys-space-sm)',
+                  }}
+                >
+                  コピー
+                </Chip>
+                <JsonFrame>
+                  {JSON.stringify(
+                    JSON.parse(CredentialsByHolder?.data[0]?.plainCredential || '{}'),
+                    null,
+                    2,
+                  )}
+                </JsonFrame>
+              </PlainCredFrame>
+            </InfoItemFrame>
+          </InfoItemsFrame>
+          <ActionFrame>
+            <Button
+              variant='outlined'
+              color='subdominant'
+              startContent={<PiX />}
+              onPress={() => router.push(`/did/${did}`)}
+              size='sm'
+            >
+              閉じる
+            </Button>
+            <Button
+              variant='tonal'
+              color='subdominant'
+              onPress={() => {
+                openURLCopied()
+                copy(`https://app.vess.id/creds/detail/${id}`)
+              }}
+              size='sm'
+            >
+              URLをコピー
+            </Button>
+          </ActionFrame>
+        </CredInfoFrame>
+      </CredDetailFrame>
+    </>
+  )
+}
+
+const CredDetailFrame = styled.div`
+  width: 100%;
+  height: 100svh;
+  display: grid;
+  grid-template-rows: 360px minmax(0, 1fr);
+  transition: all 1.5s var(--kai-motion-sys-easing-standard);
+
+  &[data-scroll-down='true'] {
+    grid-template-rows: var(--kai-size-ref-160) minmax(0, 1fr);
+  }
+`
+
+const CredImageFrame = styled.div`
+  grid-row: 1 / 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  gap: var(--kai-size-sys-space-md);
+  width: 100%;
+  padding: var(--kai-size-sys-space-xl) var(--kai-size-sys-space-md) var(--kai-size-sys-space-md);
+`
+
+const CredInfoFrame = styled.div`
+  grid-row: 2 / 3;
+  grid-column: 1 / 2;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  gap: var(--kai-size-sys-space-lg);
+  width: 100%;
+  height: auto;
+  padding: var(--kai-size-sys-space-lg) var(--kai-size-sys-space-md) var(--kai-size-sys-space-md);
+  background: var(--kai-color-sys-layer-default);
+  border-radius: var(--kai-size-sys-round-lg) var(--kai-size-sys-round-lg) 0 0;
+  border: var(--kai-size-ref-1) solid var(--kai-color-sys-neutral-outline);
+  border-bottom: none;
+  overflow: visible;
+  z-index: var(--kai-z-index-sys-overlay-default);
+`
+const InfoItemsFrame = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  gap: var(--kai-size-sys-space-lg);
+  width: 100%;
+  overflow-y: scroll;
+`
+
+const InfoItemFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: start;
+  gap: var(--kai-size-sys-space-xs);
+  width: 100%;
+`
+
+const PlainCredFrame = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+  padding: var(--kai-size-sys-space-md);
+  background: var(--kai-color-sys-layer-farthest);
+  border-radius: var(--kai-size-sys-round-md);
+  border: var(--kai-size-ref-1) solid var(--kai-color-sys-neutral-outline);
+`
+const JsonFrame = styled.pre`
+  width: 100%;
+  white-space: pre-wrap;
+  text-overflow: break-word;
+  word-break: break-all;
+  font-family: var(--kai-typo-sys-body-md-font-family);
+  font-size: var(--kai-typo-sys-body-md-font-size);
+  line-height: var(--kai-typo-sys-body-md-line-height);
+  color: var(--kai-color-sys-on-layer-minor);
+`
+
+const ActionFrame = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: var(--kai-size-sys-space-md);
+  width: 100%;
+  background: var(--kai-color-sys-layer-default);
+`
--- src/components/home/CredItem.tsx
+++ src/components/home/CredItem.tsx
@@ -1,4 +1,8 @@
+import styled from '@emotion/styled'
+import { useRouter } from 'next/router'
 import React, { FC } from 'react'
+import { Button } from 'react-aria-components'
+import { PiCheckCircleFill } from 'react-icons/pi'
 import { ImageContainer } from '../ui-v1/Images/ImageContainer'
 import { Skelton } from '@/kai/skelton'
 
@@ -6,9 +10,18 @@ type Props = {
   image?: string
   name?: string
   size?: string
+  credId?: string
 }
 
-export const CredItem: FC<Props> = ({ image, name, size = '100%' }) => {
+export const CredItem: FC<Props> = ({ image, name, size = '100%', credId }) => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    if (credId) {
+      router.push(`/creds/detail/${credId}`)
+    }
+  }
+
   return (
     <>
       <Skelton
@@ -21,14 +34,58 @@ export const CredItem: FC<Props> = ({ image, name, size = '100%' }) => {
         radius='var(--kai-size-sys-round-full)'
         borderWidth='var(--kai-size-ref-2)'
       >
-        <ImageContainer
-          src={image || '/sample/event_sample.jpg'}
-          alt={name || 'イベント参加証明画像'}
-          objectFit='cover'
-          width={size}
-          style={{ zIndex: '-1' }}
-        />
+        <CredItemFrame onPress={() => handleClick()}>
+          <ImageContainer
+            src={image || '/sample/event_sample.jpg'}
+            alt={name || 'イベント参加証明画像'}
+            objectFit='cover'
+            width={size}
+            style={{ zIndex: 0 }}
+          />
+          <IconFrame>
+            <PiCheckCircleFill size={32} color={'var(--kai-color-sys-success)'} />
+          </IconFrame>
+        </CredItemFrame>
       </Skelton>
     </>
   )
 }
+
+const CredItemFrame = styled(Button)`
+  border: none;
+  position: relative;
+  transition: background var(--kai-motion-sys-duration-fast) var(--kai-motion-sys-easing-standard);
+  transition-property: background, transform, opacity;
+  border-radius: var(--kai-size-sys-round-xs);
+  background: transparent;
+  padding: 0;
+  overflow: visible;
+
+  &[data-hovered] {
+    transform: scale(1.02);
+    cursor: pointer;
+  }
+  &[data-focused] {
+    outline: none;
+  }
+  &[data-focus-visible] {
+    outline: var(--kai-size-ref-1) solid var(--kai-color-sys-dominant);
+    outline-offset: var(--kai-size-ref-2);
+  }
+  &[data-pressed] {
+    transform: scale(0.98);
+    opacity: var(--kai-opacity-sys-state-pressed);
+  }
+`
+
+const IconFrame = styled.div`
+  position: absolute;
+  top: calc(1 * var(--kai-size-ref-12));
+  right: calc(1 * var(--kai-size-ref-16));
+  width: var(--kai-size-ref-32);
+  height: var(--kai-size-ref-32);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: var(--kai-z-index-sys-component-nearer);
+`
