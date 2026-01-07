9ba6c88dd9bf5fcb4dec225e002cf77c6665b480
Watarunto006 <wataru.udagawa.006@gmail.com>
2026-01-07 21:52:32 +0900

    page追加漏れ

---
+++ src/pages/dcapi/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import { DCApiTestPage } from '@/components/dcapi/DCApiTestPage'
+
+const DCApiPage: NextPage = () => {
+  return <DCApiTestPage />
+}
+
+export default DCApiPage
